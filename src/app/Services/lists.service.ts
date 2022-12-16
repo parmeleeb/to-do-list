695ecb490e9cc86648d790f538efec47dc69f51a
gfunue <fenue.genjang@gmail.com>
2022-12-15 21:40:00 -0600

    update

--- src/app/Services/lists.service.ts
+++ src/app/Services/lists.service.ts
@@ -8,5 +8,23 @@ export class ListsService {
 
   constructor() { }
 
-  
+  toDoList: any[] = [];
+  completedList: any[] = [];
+  favoriteList: any[] = [];
+
+  addTodoTask(taskToAdd:string, priority:boolean): void {
+    if(priority)
+      this.toDoList.unshift(taskToAdd);
+    else
+      this.toDoList.push(taskToAdd);
+  }
+
+  completeTodoTask(taskId:number): void {
+    let taskIndex = -1;
+    for(let index in this.toDoList) {
+      if(this.toDoList[index].id == taskId) taskIndex = Number(index);
+    }
+    this.completedList.push(...this.toDoList.splice(taskIndex, 1));
+  }
+
 }
